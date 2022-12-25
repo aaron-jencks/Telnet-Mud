f8f4cd8f14d647d4a2f4f14720c7f6962ee222a3
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-12-24 21:47:57 -0600

    added formatters for chat stuff

--- utils/ui/formatting.go
+++ utils/ui/formatting.go
@@ -1,9 +1,28 @@
 package ui
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 const TimestampFormat = "Mon, Jan _2 2006 @ 03:04:05 PM"
 
 func AddTimestamp(suffix string) string {
 	return time.Now().Local().Format(TimestampFormat) + suffix
+}
+
+func AddTime(suffix string) string {
+	return time.Now().Local().Format("03:04:05 PM") + suffix
+}
+
+func StripIllegalChars(data string, illegalChars string) string {
+	result := ""
+	for _, dc := range data {
+		if strings.Contains(illegalChars, fmt.Sprint(dc)) {
+			continue
+		}
+		result += fmt.Sprint(dc)
+	}
+	return result
 }
--- utils/ui/pages/chat/chat.go
+++ utils/ui/pages/chat/chat.go
@@ -1,5 +1,56 @@
 package chat
 
-func DisplayChat(entries []string) string {
+import (
+	"fmt"
+	"mud/utils/ui"
+	"mud/utils/ui/gui"
+	"strings"
+)
 
+const (
+	CHAT_H = 20
+	CHAT_W = 80
+)
+
+func FormatChatEntry(player string, entry string) string {
+	return ui.AddTime(fmt.Sprintf(" %s: %s", player, entry))
+}
+
+func DisplayChat(entries []string) string {
+	var logLines []string
+
+	// Traverse through entries backward
+	for ein := range entries {
+		ei := len(entries) - ein - 1
+
+		// Create paragraph
+		var lines []string
+		buffer := entries[ei]
+		for len(buffer) > CHAT_W-2 {
+			lines = append(lines, buffer[:CHAT_W-1])
+			buffer = buffer[CHAT_W-1:]
+		}
+		lines = append(lines, buffer)
+
+		if len(lines) > CHAT_H-2 {
+			// Truncate down to target length
+			lines = lines[len(lines)-(CHAT_H-2):]
+			logLines = lines
+			break
+		}
+
+		// Append new text history lines
+
+		logLines = append(logLines, lines...)
+
+		if len(logLines) >= CHAT_H-2 {
+			break
+		}
+	}
+
+	if len(logLines) > CHAT_H-2 {
+		logLines = logLines[len(logLines)-(CHAT_H-2):]
+	}
+
+	return gui.SizedBoxText(strings.Join(logLines, "\n"), CHAT_H, CHAT_W)
 }
