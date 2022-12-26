9d5a5d1c9a282802b79915478be997394b258a13
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-12-26 11:01:29 -0600

    updating chat box size

--- utils/consts.go
+++ utils/consts.go
@@ -6,8 +6,8 @@ var CONN_HOST string = net.GetOutboundIP().String()
 
 const (
 	SYSTEM_NAME = "SYSTEM"
-	CHAT_H      = 20
-	CHAT_W      = 80
+	CHAT_H      = 19
+	CHAT_W      = 40
 	CONN_PORT   = "23"
 	CONN_TYPE   = "tcp"
 )
