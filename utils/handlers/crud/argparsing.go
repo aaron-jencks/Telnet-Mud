2fa9c757c7d732dbf9c82d79058b145ad303077e
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2023-01-04 23:23:51 -0600

    refactoring crud utils

---
+++ utils/handlers/crud/argparsing.go
@@ -0,0 +1,73 @@
+package crud
+
+import (
+	"fmt"
+	"mud/actions/defined"
+	"mud/parsing_services/player"
+	mstrings "mud/utils/strings"
+	"net"
+	"strings"
+)
+
+func CheckArgs(conn net.Conn, args []string, target int, message string) bool {
+	username := player.GetConnUsername(conn)
+
+	if len(args) != target {
+		player.PushAction(username, defined.CreateInfoAction(conn, message))
+		return true
+	}
+	return false
+}
+
+func CheckMinArgs(conn net.Conn, args []string, target int, message string) bool {
+	username := player.GetConnUsername(conn)
+
+	if len(args) < target {
+		player.PushAction(username, defined.CreateInfoAction(conn, message))
+		return true
+	}
+	return false
+}
+
+func RequiresLoggedIn(conn net.Conn) bool {
+	username := player.GetConnUsername(conn)
+
+	if !player.ConnLoggedIn(conn) {
+		player.PushAction(username, defined.CreateInfoAction(conn, "You must be logged in to perform that action"))
+		return true
+	}
+	return false
+}
+
+func CrudChecks(conn net.Conn, crudName string, args []string) bool {
+	if RequiresLoggedIn(conn) ||
+		CheckMinArgs(conn, args, 1,
+			fmt.Sprintf("Usage: %s (create|retrieve|update|delete) ...", crudName)) ||
+		CheckStringOptions(conn, args[0], []string{"create", "retrieve", "update", "delete"},
+			fmt.Sprintf("Usage: %s operation ...", crudName), "operation") {
+		return true
+	}
+	return false
+}
+
+func ParseIntegerCheck(conn net.Conn, s string, usageString string, paramName string) (bool, int) {
+	username := player.GetConnUsername(conn)
+
+	var id int
+	_, err := fmt.Sscanf(s, "%d", &id)
+	if err != nil {
+		player.PushAction(username, defined.CreateInfoAction(conn, fmt.Sprintf("%s (%s is an integer)", usageString, paramName)))
+		return false, -1
+	}
+	return true, id
+}
+
+func CheckStringOptions(conn net.Conn, s string, options []string, usageString, paramName string) bool {
+	username := player.GetConnUsername(conn)
+
+	if !mstrings.StringContains(options, s) {
+		player.PushAction(username, defined.CreateInfoAction(conn, fmt.Sprintf("%s (%s is one of (%s)", usageString, paramName, strings.Join(options, "|"))))
+		return true
+	}
+	return false
+}
