679b99b272208bb5187c4df960fe690799f7c0e7
Aaron Jencks <aaron.jencks@syncbak.com>
2023-01-05 12:55:15 -0600

    Updated parsing for movement

--- actions/defined/movement.go
+++ actions/defined/movement.go
@@ -1,1 +1,137 @@
 package defined
+
+import (
+	"mud/actions/definitions"
+	"mud/entities"
+	"mud/parsing_services/parsing"
+	"mud/parsing_services/player"
+	"mud/services/room"
+	"net"
+	"time"
+)
+
+func CreateMoveUpAction(conn net.Conn) definitions.Action {
+	return definitions.Action{
+		Name:       "Move Up",
+		Duration:   100 * time.Millisecond,
+		ValidModes: []string{"Logged In"},
+		Handler: func() parsing.CommandResponse {
+			username := player.GetConnUsername(conn)
+			p := player.CRUD.Retrieve(username).(entities.Player)
+
+			if p.RoomY > 0 {
+				p.RoomY--
+				player.CRUD.Update(username, p)
+
+				return parsing.CommandResponse{
+					LoggedIn: true,
+					Map:      true,
+					Person:   true,
+				}
+			}
+
+			player.PushAction(username, CreateInfoAction(conn, "You're at the edge of the room"))
+
+			return parsing.CommandResponse{
+				LoggedIn: true,
+				Info:     true,
+				Person:   true,
+			}
+		},
+	}
+}
+
+func CreateMoveLeftAction(conn net.Conn) definitions.Action {
+	return definitions.Action{
+		Name:       "Move Left",
+		Duration:   100 * time.Millisecond,
+		ValidModes: []string{"Logged In"},
+		Handler: func() parsing.CommandResponse {
+			username := player.GetConnUsername(conn)
+			p := player.CRUD.Retrieve(username).(entities.Player)
+
+			if p.RoomX > 0 {
+				p.RoomX--
+				player.CRUD.Update(username, p)
+
+				return parsing.CommandResponse{
+					LoggedIn: true,
+					Map:      true,
+					Person:   true,
+				}
+			}
+
+			player.PushAction(username, CreateInfoAction(conn, "You're at the edge of the room"))
+
+			return parsing.CommandResponse{
+				LoggedIn: true,
+				Info:     true,
+				Person:   true,
+			}
+		},
+	}
+}
+
+func CreateMoveRightAction(conn net.Conn) definitions.Action {
+	return definitions.Action{
+		Name:       "Move Right",
+		Duration:   100 * time.Millisecond,
+		ValidModes: []string{"Logged In"},
+		Handler: func() parsing.CommandResponse {
+			username := player.GetConnUsername(conn)
+			p := player.CRUD.Retrieve(username).(entities.Player)
+			r := room.CRUD.Retrieve(p.Room).(entities.Room)
+
+			if p.RoomX < r.Width-1 {
+				p.RoomX++
+				player.CRUD.Update(username, p)
+
+				return parsing.CommandResponse{
+					LoggedIn: true,
+					Map:      true,
+					Person:   true,
+				}
+			}
+
+			player.PushAction(username, CreateInfoAction(conn, "You're at the edge of the room"))
+
+			return parsing.CommandResponse{
+				LoggedIn: true,
+				Info:     true,
+				Person:   true,
+			}
+		},
+	}
+}
+
+func CreateMoveDownAction(conn net.Conn) definitions.Action {
+	return definitions.Action{
+		Name:       "Move Down",
+		Duration:   100 * time.Millisecond,
+		ValidModes: []string{"Logged In"},
+		Handler: func() parsing.CommandResponse {
+			username := player.GetConnUsername(conn)
+			p := player.CRUD.Retrieve(username).(entities.Player)
+			r := room.CRUD.Retrieve(p.Room).(entities.Room)
+
+			if p.RoomY < r.Height-1 {
+				p.RoomY++
+				player.CRUD.Update(username, p)
+
+				return parsing.CommandResponse{
+					LoggedIn: true,
+					Map:      true,
+					Person:   true,
+				}
+			}
+
+			player.PushAction(username, CreateInfoAction(conn, "You're at the edge of the room"))
+
+			return parsing.CommandResponse{
+				LoggedIn: true,
+				Info:     true,
+				Person:   true,
+			}
+		},
+	}
+}
--- actions/handlers/movement.go
+++ actions/handlers/movement.go
@@ -1,17 +1,27 @@
 package handlers
 
 import (
-	"mud/entities"
+	"mud/actions/defined"
+	"mud/parsing_services/player"
+	"net"
 )
 
-func HandleUpMovement(player entities.Player) {
+func HandleUpMovement(conn net.Conn) {
+	username := player.GetConnUsername(conn)
+	player.PushAction(username, defined.CreateMoveUpAction(conn))
 }
 
-func HandleLeftMovement(player entities.Player) {
+func HandleLeftMovement(conn net.Conn) {
+	username := player.GetConnUsername(conn)
+	player.PushAction(username, defined.CreateMoveLeftAction(conn))
 }
 
-func HandleRightMovement(player entities.Player) {
+func HandleRightMovement(conn net.Conn) {
+	username := player.GetConnUsername(conn)
+	player.PushAction(username, defined.CreateMoveRightAction(conn))
 }
 
-func HandleDownMovement(player entities.Player) {
+func HandleDownMovement(conn net.Conn) {
+	username := player.GetConnUsername(conn)
+	player.PushAction(username, defined.CreateMoveDownAction(conn))
 }
