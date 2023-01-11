37eff5fc6398c6da09ce777ac0fd9c24904a12a2
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2023-01-10 18:53:53 -0600

    Removed locks from queue

--- actions/definitions/queue.go
+++ actions/definitions/queue.go
@@ -1,35 +1,21 @@
 package definitions
 
-import (
-	"sync"
-)
-
 type ActionQueue struct {
-	Contents       chan Action
-	MaxLength      int
-	QueueWriteLock sync.Mutex
-	QueueReadLock  sync.Mutex
+	Contents  chan Action
+	MaxLength int
 }
 
 func CreateActionQueue(maxCapacity int) *ActionQueue {
 	return &ActionQueue{
-		Contents:       make(chan Action, maxCapacity),
-		MaxLength:      maxCapacity,
-		QueueWriteLock: sync.Mutex{},
-		QueueReadLock:  sync.Mutex{},
+		Contents:  make(chan Action, maxCapacity),
+		MaxLength: maxCapacity,
 	}
 }
 
 func (q *ActionQueue) Enqueue(a Action) {
-	q.QueueWriteLock.Lock()
-	defer q.QueueWriteLock.Unlock()
-
 	q.Contents <- a
 }
 
 func (q *ActionQueue) Dequeue() Action {
-	q.QueueReadLock.Lock()
-	defer q.QueueReadLock.Unlock()
-
 	return <-q.Contents
 }
